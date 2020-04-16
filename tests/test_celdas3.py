667752acce55766d934110d76da9863027574e36
CamiloJ <camijimemo@gmail.com>
2020-04-16 16:16:19 -0300

    Comento y dejo mas ordenado el codigo

--- tests/test_celdas3.py
+++ tests/test_celdas3.py
@@ -2,10 +2,15 @@ from src.bingo import carton
 
 def test3():
     tarea3 = carton()
+
+    #variables con la primera tupla (0) , segunda (1), tercera (2)
     mi_carton1=tarea3[0]
     mi_carton2=tarea3[1]
     mi_carton3=tarea3[2]
+
+    #Variable para contar si hay alguna celda ocupada en la columna
     i=0
+
     #El zip logra hacer una lista de tuplas y el x y z representan al primero, segundo y tercero de la tupla respectivamente
     for x, y, z  in zip(mi_carton1,mi_carton2,mi_carton3):
         if (x==1 or y==1 or z==1):
--- testsFilas/test_filas1.py
+++ testsFilas/test_filas1.py
@@ -2,12 +2,18 @@ from src.bingo import carton
 
 def test4():
     tarea4 = carton()
+    
+    #Agrego variables para que tengan las tuplas correspondientes de carton()
     mi_carton1=tarea4[0]
     mi_carton2=tarea4[1]
     mi_carton3=tarea4[2]
+
+    #Contadores para saber si hay o no 1 en las filas
     a=0
     j=0
     k=0
+
+    #Los fors recorren la tupla buscando almenos 1 celda ocupada de la fila
     for x in mi_carton1:
         a += x
     for z in mi_carton2:

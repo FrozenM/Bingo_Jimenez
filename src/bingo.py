cfaf7a03490d7515f700fc2147463612ad0ee149
CamiloJ <camijimemo@gmail.com>
2020-04-16 16:03:05 -0300

    Pongo la cantidad de celdas en 15 otra vez para que pasen todos los tests

--- src/bingo.py
+++ src/bingo.py
@@ -3,7 +3,7 @@ def carton():
     # 0 representa vacio, 1 representa ocupado
     mi_carton = (
         (1,0,1,1,1,1,1,1,1),
-        (1,1,1,1,1,1,1,0,0),
+        (1,1,1,1,1,1,0,0,0),
         (0,0,0,0,0,0,1,0,0)
     )
     return mi_carton

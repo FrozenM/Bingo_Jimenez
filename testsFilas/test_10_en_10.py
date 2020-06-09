2c1ef9666ad235611df02a3e093beeec0df06a55
CamiloJ <camijimemo@gmail.com>
2020-06-09 08:36:50 -0300

    Borro tests para dejar todo mas ordenado

--- testsFilas/test_10_en_10.py
+++
@@ -1,9 +0,0 @@
-from src.bingo import carton
-
-def test_diez_en_diez():
-    prueba = carton()
-
-    #variables con la primera tupla (0) , segunda (1), tercera (2)
-    mi_carton1=prueba[0]
-    mi_carton2=prueba[1]
-    mi_carton3=prueba[2]
